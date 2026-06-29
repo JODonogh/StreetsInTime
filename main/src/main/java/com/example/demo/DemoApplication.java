25ac9e66c82832b79a37ffb2a83f7c68a9161317
JODonogh <98493577+JODonogh@users.noreply.github.com>
2026-06-29 21:56:35 +0000

    application rename

--- main/src/main/java/com/example/demo/DemoApplication.java
+++ main/src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class StreetsInTimeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(StreetsInTimeApplication.class, args);
 	}
 
 }
